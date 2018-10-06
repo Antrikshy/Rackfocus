047015471824abd228afd9e58afbb21bd3501328
Antriksh Yadav <antrikshy@gmail.com>
2018-10-06 15:40:22 -0700

    Packaging project and making it installable using setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rackfocus',
+    version='1.0',
+    author='Antriksh Yadav',
+    author_email='antrikshy@gmail.com',
+    description=('CLI utility to download and compile '
+                 'IMDb datasets into an SQLite database.'),
+    long_description=long_description,
+    url='https://github.com/Antrikshy/Rackfocus',
+    entry_points = {
+        'console_scripts': [
+            'rackfocus=rackfocus.run:main'
+        ]
+    },
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Database'
+    ],
+)
